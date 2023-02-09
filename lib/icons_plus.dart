235e287b85a0e6ea527e0210fb3e52d07a27d2f7
Rahul Chouhan <rahul-chouhan@outlook.com>
2023-02-09 10:16:20 +0530

    All exports have been removed.

--- lib/icons_plus.dart
+++ lib/icons_plus.dart
@@ -1,11 +1,3 @@
 library icons_plus;
 
-export 'src/bootstrap_icons.dart';
-export 'src/boxicons_icons.dart';
-export 'src/eva_icons.dart';
-export 'src/flag.dart';
-export 'src/fontawesome_icons.dart';
-export 'src/ionicons_icons.dart';
-export 'src/lineawesome_icons.dart';
-export 'src/pixelarticons_icons.dart';
-export 'src/logo.dart';
+export 'src/src.dart';
